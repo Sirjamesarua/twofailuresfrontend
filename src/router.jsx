3a6c44bc5304a078eb809852aa7668a4f6ae519f
Ernest Haruna <92730168+ernestharuna@users.noreply.github.com>
2023-08-21 22:27:37 +0100

    abracadabra

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import GuestLayout from "./layouts/GuestLayout";
 import Home from "./views/user/home/Home";
 import Episode from "./views/user/episode/Episode";
+import AdminLayout from "./layouts/AdminLayout";
+import AdminDashboard from "./views/admin/AdminDashboard";
+import EpLayout from "./views/admin/episodes/EpLayout";
+import AllEpisodes from "./views/admin/episodes/AllEpisodes";
+import CreateEpisode from "./views/admin/episodes/CreateEpisode";
+import EditEpisode from "./views/admin/episodes/EditEpisode";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +21,35 @@ const router = createBrowserRouter([
             {
                 path: 'read/episode/',
                 element: <Episode />
+            }
+        ]
+    },
+    {
+        path: '/admin',
+        element: <AdminLayout />,
+        children: [
+            {
+                index: true,
+                path: "dashboard",
+                element: <AdminDashboard />,
+            },
+            {
+                path: "episodes",
+                element: <EpLayout />,
+                children: [
+                    {
+                        index: true,
+                        element: <AllEpisodes />
+                    },
+                    {
+                        path: "create",
+                        element: <CreateEpisode />
+                    },
+                    {
+                        path: "edit/:episodeId",
+                        element: <EditEpisode />
+                    }
+                ]
             }
         ]
     }
