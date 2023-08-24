ade35a3f1ebeb34e1c1dd6d2ccf8e6bf7a162842
Ernest Haruna <92730168+ernestharuna@users.noreply.github.com>
2023-08-24 14:53:35 +0100

    CRUD for Ads

--- src/views/admin/adverts/EditAd.jsx
+++ src/views/admin/adverts/EditAd.jsx
@@ -1,29 +1,41 @@
 import { useForm } from "react-hook-form";
 import 'react-quill/dist/quill.snow.css';
 import axiosClient from "../../../axios-client";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import axiosClient2 from "../../../axios-client-2";
 
 export async function loader({ params }) {
     const { data } = await axiosClient.get(`/admin/adverts/${params.adId}`);
     const ad = data;
-    console.log(ad);
     return ad;
 }
 
 export default function EditAd() {
     const ad = useLoaderData();
-    const { register, handleSubmit, formState: { isSubmitting } } = useForm();
+    // const navigate = useNavigate();
+    const { handleSubmit, formState: { isSubmitting } } = useForm();
     const [advert, setAdvert] = useState({
         name: ad.name,
         link: ad.link,
-        image: ad.image
+        image: null
     });
 
-    const onSubmit = async (data) => {
-        console.log(data);
+    const onSubmit = async () => {
+        console.log(advert);
+        await axiosClient2.post(`/admin/adverts/${ad.id}`, advert)
+            .then(() => {
+                alert("advert updated!");
+            }).catch((err) => {
+                console.log(err);
+                // alert("An error occured!");
+            })
     }
 
+    const handleImageChange = (ev) => {
+        const file = ev.target.files;
+        setAdvert({ ...advert, image: file })
+    }
 
     return (
         <div>
@@ -31,8 +43,7 @@ export default function EditAd() {
             <form onSubmit={handleSubmit(onSubmit)} id="createEpForm">
                 <div className="input-control">
                     <label htmlFor="name">Advert Name</label><br />
-                    <input type="text" id="name" value={ad.name}
-                        {...register("name", { required: true })}
+                    <input type="text" id="name" value={advert.name}
                         onChange={e => setAdvert({
                             ...advert, name: e.target.value
                         })}
@@ -41,8 +52,7 @@ export default function EditAd() {
 
                 <div className="input-control">
                     <label htmlFor="link">Goto Link</label><br />
-                    <input type="text" id="link" value={ad.link}
-                        {...register("link", { required: true })}
+                    <input type="text" id="link" value={advert.link}
                         onChange={e => setAdvert({
                             ...advert, link: e.target.value
                         })}
@@ -50,17 +60,14 @@ export default function EditAd() {
                 </div>
 
                 <div className="input-control">
-                    <label htmlFor="image">Image</label><br />
+                    <label htmlFor="image">Change Image</label><br />
                     <input type="file" id="image"
-                        {...register('image', { required: true })}
-                        onChange={e => setAdvert({
-                            ...advert, image: e.target.value
-                        })}
+                        onChange={handleImageChange}
                     />
                 </div>
 
                 <div style={{ width: "auto", border: "2px solid grey" }}>
-                    <img src={`${import.meta.env.VITE_IMAGE_URL}/${ad.image}`} alt="advert image" />
+                    <img src={`${import.meta.env.VITE_IMAGE_URL}/adverts/${ad.image}`} alt="advert image" />
                 </div>
 
                 <div className="input-control mt-1">
