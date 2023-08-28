65375db46bff5196ecfc95d8ab67d7423e4ae88d
Ernest Haruna <92730168+ernestharuna@users.noreply.github.com>
2023-08-28 16:21:17 +0100

    Made error page display actual route errors

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 export default function ErrorPage() {
+    const error = useRouteError();
+    console.log(error);
+
     return (
         <div>
             <div className="container mt-2 text-center">
                 <p>Error Occured</p>
+                <p>
+                    <i>{error.statusText || error.message}</i>
+                </p>
                 <button className="err-btn" onClick={() => window.location.reload()}>
                     Reload
                 </button>
