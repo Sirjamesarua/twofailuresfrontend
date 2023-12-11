a0f0595410710259c61c36007e9dd48125f232ca
Ernest Haruna <ernestharuna1@gmail.com>
2023-12-11 12:35:46 +0100

    Added text to Login pop

--- src/components/LoginPop.jsx
+++ src/components/LoginPop.jsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import axiosClient from "../axios-client.js";
 import Backdrop from "./Backdrop";
-import { useStateContext } from "../context/ContextProvider.jsx";   
+import { useStateContext } from "../context/ContextProvider.jsx";
 import HCaptcha from "@hcaptcha/react-hcaptcha";
 import { useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
@@ -12,7 +12,7 @@ export default function LoginPop() {
     const { register, handleSubmit, formState: { isSubmitting } } = useForm();
     const { putUser, redirect } = useStateContext()
     const navigate = useNavigate();
-    
+
     const [isCaptchaVerified, setIsCaptchaVerified] = useState(false);
 
     // const onSubmit = async (email) => {
@@ -36,13 +36,13 @@ export default function LoginPop() {
     //             alert("Something went wrong" + response)
     //         });
     // }
-    
+
     const onSubmit = async (email) => {
         await axiosClient.post('/login', email)
             .then(({ data }) => {
                 console.log(data);
-                    putUser(data.user.email)
-                    navigate(redirect)
+                putUser(data.user.email)
+                navigate(redirect)
             })
             .catch(({ response }) => {
                 const res = response.data.message;
@@ -53,10 +53,10 @@ export default function LoginPop() {
     const onVerify = (token) => {
         // Handle the hCaptcha token (e.g., send it to your server for verification)
         console.log('hCaptcha Token:', token);
-        
+
         setIsCaptchaVerified(true);
-      };
-      
+    };
+
     const onChange = (value) => {
         console.log("Captcha value:", value);
         setIsCaptchaVerified(true);
@@ -76,9 +76,9 @@ export default function LoginPop() {
                     <div className="input-control">
                         <label htmlFor="email">
                             <small>
-                                Please enter a valid email address to continue <br />
+                                Please enter a valid email address to continue. <br />
+                                You will get notified of new episodes via Email. <br />
                             </small>
-                            <br />
                             <small className="color-blue">
                                 <b>You only have to do this once</b>
                             </small>
