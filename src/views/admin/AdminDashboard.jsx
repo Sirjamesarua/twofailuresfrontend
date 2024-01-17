5c6f7364e01b8199c7a9f0b280eea224450c7eba
Ernest Haruna <ernestharuna1@gmail.com>
2024-01-17 16:05:50 +0100

    Added html for admin to view visits

--- src/views/admin/AdminDashboard.jsx
+++ src/views/admin/AdminDashboard.jsx
@@ -4,8 +4,28 @@ export async function loader() {
 
 export default function AdminDashboard() {
   return (
-    <div className="admin-dashboard container">
-      admin content goes here
+    <div className="admin-dashboard container" id="admin-dashobard">
+      <section className="d-flex">
+        <div className="leads p-2">
+          <div>
+            Facebook visits
+          </div>
+          <hr />
+          <div>
+            233,231
+          </div>
+        </div>
+
+        <div className="leads p-2">
+          <div>
+            Reddit visits
+          </div>
+          <hr />
+          <div>
+            233,231
+          </div>
+        </div>
+      </section >
     </div>
   )
 }
