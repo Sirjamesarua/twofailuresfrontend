2c8076bfd19e6a7fd00d017ea2f54fcb6573b8d5
jamesarua <dev.sirjames@gmail.com>
2024-02-01 11:07:40 +0100

    sss

--- src/views/admin/episodes/EditEpisode.jsx
+++ src/views/admin/episodes/EditEpisode.jsx
@@ -101,13 +101,32 @@ export default function EditEpisode() {
 
     }
 
-    const toolbarOptions = [
-        [{ 'header': [3, false] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'], // text formatting options
-        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
-        ['link', 'image'],
-        ['clean']
-    ];
+    // const toolbarOptions = [
+    //     [{ 'header': [3, false] }],
+    //     ['bold', 'italic', 'underline', 'strike', 'blockquote'], // text formatting options
+    //     [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+    //     ['link', 'image'],
+    //     ['clean']
+    // ];
+    var toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+      
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+      
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+      
+        ['clean']                                         // remove formatting button
+      ];
 
     return (
         <div>
